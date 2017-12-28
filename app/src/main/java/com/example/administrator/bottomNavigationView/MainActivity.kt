7962788f232801a7277c191b5107c69f2c7bb5e0
Jamel <1136650395@qq.com>
2017-12-29 05:14:53 +0800

    2017/12/29 code settings icon

--- app/src/main/java/com/example/administrator/bottomNavigationView/MainActivity.kt
+++ app/src/main/java/com/example/administrator/bottomNavigationView/MainActivity.kt
@@ -15,17 +15,34 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         initViewPagerAndBottomNavigationView()
-        //tabLayout绑定viewPager
-        tabLayout.setupWithViewPager(viewPager)
+        initTabLayout()
     }
     /**
-     * 初始化viewpager和BottomNavigationView,并联动，关闭MenuItem滑动动画
+    * 初始化tabLayout并与viewpager联动
     * */
+    private fun initTabLayout() {
+        //tabLayout添加tab
+        tabLayout.addTab(tabLayout.newTab())
+        tabLayout.addTab(tabLayout.newTab())
+        tabLayout.addTab(tabLayout.newTab())
+        tabLayout.addTab(tabLayout.newTab())
+        //tabLayout绑定viewPager
+        tabLayout.setupWithViewPager(viewPager)
+        //tab设置文字、图片
+        tabLayout.getTabAt(0)?.setText(R.string.alarm)?.setIcon(R.drawable.alarm)
+        tabLayout.getTabAt(1)?.setText(R.string.collect)?.setIcon(R.drawable.collect)
+        tabLayout.getTabAt(2)?.setText(R.string.gallery)?.setIcon(R.drawable.gallery)
+        tabLayout.getTabAt(3)?.setText(R.string.mine)?.setIcon(R.drawable.mine)
+    }
+
+    /**
+     * 初始化viewpager和BottomNavigationView,并联动，关闭MenuItem滑动动画
+     * */
     @SuppressLint("RestrictedApi")
     private fun initViewPagerAndBottomNavigationView() {
         //viewpager适配器
         val fragments = arrayOf(BlankFragment(), BlankFragment2(), BlankFragment3(), BlankFragment4())
-        viewPager.adapter = MyFragmentPagerAdapter(fragments, supportFragmentManager,this)
+        viewPager.adapter = MyFragmentPagerAdapter(fragments, supportFragmentManager)
         //MenuItem只能有一个是选中状态
         viewPager.addOnPageChangeListener(object : ViewPager.OnPageChangeListener {
             override fun onPageScrollStateChanged(state: Int) {
--- app/src/main/java/com/example/administrator/bottomNavigationView/MyFragmentPagerAdapter.kt
+++ app/src/main/java/com/example/administrator/bottomNavigationView/MyFragmentPagerAdapter.kt
@@ -1,16 +1,12 @@
 package com.example.administrator.bottomNavigationView
 
-import android.content.Context
 import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentManager
 import android.support.v4.app.FragmentPagerAdapter
-import android.support.v4.content.ContextCompat
-import android.text.SpannableString
-import android.text.style.ImageSpan
 
 
-class MyFragmentPagerAdapter(private val fragments: Array<Fragment>, fragmentManager: FragmentManager, private val context: Context) : FragmentPagerAdapter(fragmentManager) {
-    private val imageIds: IntArray = intArrayOf(R.drawable.alarm, R.drawable.collect, R.drawable.gallery, R.drawable.mine)
+class MyFragmentPagerAdapter(private val fragments: Array<Fragment>, fragmentManager: FragmentManager) : FragmentPagerAdapter(fragmentManager) {
+
     override fun getItem(position: Int): Fragment {
         return fragments[position]
     }
@@ -19,15 +15,5 @@ class MyFragmentPagerAdapter(private val fragments: Array<Fragment>, fragmentMan
         return fragments.size
     }
 
-    override fun getPageTitle(position: Int): CharSequence? {
-//        纯字tab
-//        return context.resources.getStringArray(R.array.tab)[position]
-//        纯图tab
-        val drawable = ContextCompat.getDrawable(context, imageIds[position])
-        drawable?.setBounds(0, 0, drawable.intrinsicWidth, drawable.intrinsicWidth)
-        val imageSpan = ImageSpan(drawable, ImageSpan.ALIGN_BOTTOM)
-        val spannableString = SpannableString(" ")
-        spannableString.setSpan(imageSpan, 0, 1, SpannableString.SPAN_EXCLUSIVE_EXCLUSIVE)
-        return spannableString
-    }
+
 }
